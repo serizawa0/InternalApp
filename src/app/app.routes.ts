51531a9d8e22cdb2045c6a78bb85fac0fc9ada75
serizawa0 <rakotomangatsilavina@gmail.com>
2025-10-20 12:24:09 +0300

    12:24

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Principal } from './pages/principal/principal';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'', component:Principal
+  }
+];
